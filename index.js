c0c0e6d98dfe1ae0800a691447fdfccaea191b4b
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-05-23 16:49:13 +0600

    Parts call data API created

--- index.js
+++ index.js
@@ -12,14 +12,24 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://NASAH_DBA:<password>@cluster0.y9rmt.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y9rmt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('Database connection done')
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db('NBSP-database').collection('parts');
+
+        app.get('/parts', async (req, res) => {
+            const parts = await partsCollection.find({}).toArray();
+            res.send(parts);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
