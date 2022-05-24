cb0c4a108ea00f573c40ab8a646cd04bfdf9474e
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-05-24 20:38:26 +0600

    API created to check is admin or not

--- index.js
+++ index.js
@@ -71,6 +71,14 @@ async function run() {
             res.send({ result, token });
         })
 
+        // Check admin or not
+        app.get('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin';
+            res.send(isAdmin)
+        })
+
     }
     finally {
 
